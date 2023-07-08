5db039b1579dd8084c727b5bd9038f812f380bda
mihaicc30 <alemihai25@gmail.com>
2023-07-08 06:41:21 +0100

    added payment animation

--- src/comp/payment/Payment.jsx
+++ src/comp/payment/Payment.jsx
@@ -20,6 +20,7 @@ const stripePromise = loadStripe(import.meta.env.VITE_S_PK);
 const Payment = ({ user, basketItems, setBasketItems, venueNtable, setVenueNtable }) => {
   const [isCardPressed, setIsCardPressed] = useState(false);
   const [isCashPressed, setIsCashPressed] = useState(false);
+  const [showCardSwipe, setshowCardSwipe] = useState(false);
 
   const nav = useNavigate();
   const [computedBasket, setComputedBasket] = useState([]);
@@ -78,28 +79,40 @@ const Payment = ({ user, basketItems, setBasketItems, venueNtable, setVenueNtabl
         setStripeResponse(``);
       }, 1400);
     } else if (type === "card") {
+      setshowCardSwipe(!showCardSwipe);
       setIsCardPressed(true);
-      await stripePayment().then(async () => {
-        let payingAmount = padInput;
-        let payIntoBill = (parseFloat(paymentTaken) + parseFloat(payingAmount)).toFixed(2);
+      try {
+        const response = await stripePayment();
+        if (!response) {
+          setshowCardSwipe(false);
+          console.log("paymentIntent", response);
+          setStripeResponse(`Fail! Could not get stripe payment intent.`);
+          setTimeout(() => {
+            setStripeResponse(``);
+          }, 1400);
+          setIsCardPressed(false);
+          return;
+        }
+        setshowCardSwipe(false);
+        const payingAmount = padInput;
+        const payIntoBill = (parseFloat(paymentTaken) + parseFloat(payingAmount)).toFixed(2);
         setpaymentTaken(payIntoBill);
-        let leftToPay = (parseFloat(basketTotal) - parseFloat(payIntoBill)).toFixed(2);
-        let change = -(parseFloat(basketTotal) - parseFloat(payIntoBill)).toFixed(2);
+        const leftToPay = (parseFloat(basketTotal) - parseFloat(payIntoBill)).toFixed(2);
+        const change = -(parseFloat(basketTotal) - parseFloat(payIntoBill)).toFixed(2);
         setChange(change);
-        setStripeResponse(`Payment successful! £${padInput} has been paid. ${leftToPay > 0 ? `£${leftToPay} are left to pay .` : ``}`);
-
+        setStripeResponse(`Success! Payment of £${padInput} has been made. ${leftToPay > 0 ? `£${leftToPay} are left to pay.` : ``}`);
         setTimeout(() => {
           setStripeResponse(``);
         }, 1400);
         setIsCardPressed(false);
         if (leftToPay <= 0) {
-          console.log("Basked is Paid. Clearing the basket.");
-
+          console.log("Basket is paid. Clearing the basket.");
           setBasketItems([]);
         }
-
-        console.log("so fast ?");
-      });
+        console.log("Payment successful:", response);
+      } catch (error) {
+        console.log("Payment failed:", error);
+      }
     }
   };
 
@@ -282,11 +295,253 @@ const Payment = ({ user, basketItems, setBasketItems, venueNtable, setVenueNtabl
         </button>
 
         <button disabled={DCOPB()} onClick={() => handlePaymentType("card")} className={`text-xl m-2 px-6 py-4 border-b-2 border-b-black relative basis-[20%] transition-all cursor-pointer hover:scale-[0.98] active:scale-[0.90] rounded-xl flex flex-col text-center justify-center font-semibold ${DCOPB() ? "bg-gray-300 text-gray-400" : "bg-[--c1]"} `}>
-          Confirm {padInput.length !== 0 && parseFloat(basketTotal) > parseFloat(paymentTaken) ? "£" : ""}
-          {parseFloat(basketTotal) > parseFloat(paymentTaken) ? padInput : ""} Payment Taken with Card <SiContactlesspayment className="text-[4rem] stroke-[0.1px] mx-auto" />
+          {!showCardSwipe 
+          ?  
+          <>
+          Take {padInput.length !== 0 && parseFloat(basketTotal) > parseFloat(paymentTaken) ? "£" : ""}
+          {parseFloat(basketTotal) > parseFloat(paymentTaken) ? padInput : ""} Card Payment
+          </>
+          :
+          <>
+         <AiOutlineLoading3Quarters className="animate-spin mx-auto text-5xl" />
+          </>
+          }
+           
+          
+          <SiContactlesspayment className="text-[4rem] stroke-[0.1px] mx-auto" />
         </button>
       </div>
-      <span></span> {/* placeholder for future development */}
+      <span>
+      {showCardSwipe && (
+        <div className="">
+          <div className="animated-bg-container">
+            <div className="background-image"></div>
+          </div>
+          <div className="icon-container2">
+            <div className="icon2">
+              <div className="inner-icon2">
+                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 756 858" enableBackground="new 0 0 756 858" xmlSpace="preserve">
+                  <path
+                    id="bottomslide"
+                    fill="#fff"
+                    d="M495.8,541.9h-84.5c-1.1,0-2-0.9-2-2V342.5c0-1.1,0.9-2,2-2h84.5c1.1,0,2,0.9,2,2v197.5
+               C497.8,541,496.9,541.9,495.8,541.9z"
+                  />
+                  <g id="card2" v-bind:classname="{'animate':step === 'stepfive'}">
+                    <path
+                      fill="#E5E5E5"
+                      d="M556,123.2v199.9c0,1.1-0.9,2-2,2H430.7c-1.1,0-2-0.9-2-2V123.2c0-1.1,0.9-2,2-2H554
+                  C555.1,121.2,556,122.1,556,123.2z"
+                    />
+                    <rect x="351.2" y="209.5" transform="matrix(6.123234e-17 -1 1 6.123234e-17 230.0159 676.2959)" fill="#303030" width="203.9" height="27.2" />
+                    <rect x="430.5" y="213.9" transform="matrix(6.123234e-17 -1 1 6.123234e-17 274.1384 720.4184)" fill="#FFFFFF" width="133.5" height="18.5" />
+                    <rect x="484.3" y="171.1" transform="matrix(6.123234e-17 -1 1 6.123234e-17 323.1085 671.4483)" fill="#A0A0A0" width="26" height="6.2" />
+                    <rect x="528.8" y="288.5" transform="matrix(6.123234e-17 -1 1 6.123234e-17 250.194 833.3431)" fill="#A0A0A0" width="26" height="6.2" />
+                    <rect x="501.6" y="226.7" transform="matrix(6.123234e-17 -1 1 6.123234e-17 311.9859 771.5512)" fill="#A0A0A0" width="80.3" height="6.2" />
+                  </g>
+                  <g id="machine">
+                    <path
+                      id="topslide"
+                      fill="#f88f2d"
+                      d="M483.6,546.2h-84.5c-1.1,0-2-0.9-2-2v-206c0-1.1,0.9-2,2-2h84.5c1.1,0,2,0.9,2,2v206
+                  C485.6,545.3,484.7,546.2,483.6,546.2z"
+                    />
+                    <path
+                      fill="#fa8d2883"
+                      d="M426.6,794.8h-304c-1.1,0-2-0.9-2-2V316.7c0-1.1,0.9-2,2-2h304c1.1,0,2,0.9,2,2v476.1
+                  C428.6,793.9,427.7,794.8,426.6,794.8z"
+                    />
+                    <path
+                      fill="#57412a"
+                      d="M422.6,786.2H126c-3.3,0-6-2.7-6-6V271.5c0-3.3,2.7-6,6-6h296.6c3.3,0,6,2.7,6,6v508.8
+                  C428.6,783.5,425.9,786.2,422.6,786.2z"
+                    />
+                    <rect x="120" y="295.5" fill="#fa8d2883" width="308.6" height="495.1" />
+                    <rect x="146.8" y="295.5" fill="#f88f2d" width="255" height="499.3" />
+                    <rect x="171.4" y="325.5" fill="#F9F9F9" width="205.7" height="158.6" />
+                    <g>
+                      <g>
+                        <path
+                          fill="#fff"
+                          d="M229.4,537.6H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,536.7,230.5,537.6,229.4,537.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M302.6,537.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,536.7,303.7,537.6,302.6,537.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M375.8,537.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,536.7,376.9,537.6,375.8,537.6z"
+                        />
+                      </g>
+                      <g>
+                        <path
+                          fill="#fff"
+                          d="M229.4,582.6H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,581.7,230.5,582.6,229.4,582.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M302.6,582.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,581.7,303.7,582.6,302.6,582.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M375.8,582.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,581.7,376.9,582.6,375.8,582.6z"
+                        />
+                      </g>
+                      <g>
+                        <path
+                          fill="#fff"
+                          d="M229.4,627.6H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,626.7,230.5,627.6,229.4,627.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M302.6,627.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,626.7,303.7,627.6,302.6,627.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M375.8,627.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,626.7,376.9,627.6,375.8,627.6z"
+                        />
+                      </g>
+                      <g>
+                        <path
+                          fill="#fff"
+                          d="M229.4,672.6H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,671.7,230.5,672.6,229.4,672.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M302.6,672.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,671.7,303.7,672.6,302.6,672.6z"
+                        />
+                        <path
+                          fill="#fff"
+                          d="M375.8,672.6h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,671.7,376.9,672.6,375.8,672.6z"
+                        />
+                      </g>
+                    </g>
+                    <g>
+                      <g>
+                        <path
+                          fill="#57412a"
+                          d="M229.4,531.2H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,530.3,230.5,531.2,229.4,531.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M302.6,531.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,530.3,303.7,531.2,302.6,531.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M375.8,531.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,530.3,376.9,531.2,375.8,531.2z"
+                        />
+                      </g>
+                      <g>
+                        <path
+                          fill="#57412a"
+                          d="M229.4,576.2H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,575.3,230.5,576.2,229.4,576.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M302.6,576.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,575.3,303.7,576.2,302.6,576.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M375.8,576.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,575.3,376.9,576.2,375.8,576.2z"
+                        />
+                      </g>
+                      <g>
+                        <path
+                          fill="#57412a"
+                          d="M229.4,621.2H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,620.3,230.5,621.2,229.4,621.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M302.6,621.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,620.3,303.7,621.2,302.6,621.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M375.8,621.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,620.3,376.9,621.2,375.8,621.2z"
+                        />
+                      </g>
+                      <g>
+                        <path
+                          fill="#57412a"
+                          d="M229.4,666.2H174c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C231.4,665.3,230.5,666.2,229.4,666.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M302.6,666.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C304.6,665.3,303.7,666.2,302.6,666.2z"
+                        />
+                        <path
+                          fill="#57412a"
+                          d="M375.8,666.2h-55.3c-1.1,0-2-0.9-2-2v-21.7c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v21.7
+                        C377.8,665.3,376.9,666.2,375.8,666.2z"
+                        />
+                      </g>
+                    </g>
+                    <g>
+                      <path
+                        fill="#C6C6C6"
+                        d="M229.4,743.4H174c-1.1,0-2-0.9-2-2v-28.1c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v28.1
+                     C231.4,742.5,230.5,743.4,229.4,743.4z"
+                      />
+                      <path
+                        fill="#C6C6C6"
+                        d="M302.6,743.4h-55.3c-1.1,0-2-0.9-2-2v-28.1c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v28.1
+                     C304.6,742.5,303.7,743.4,302.6,743.4z"
+                      />
+                      <path
+                        fill="#C6C6C6"
+                        d="M375.8,743.4h-55.3c-1.1,0-2-0.9-2-2v-28.1c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v28.1
+                     C377.8,742.5,376.9,743.4,375.8,743.4z"
+                      />
+                    </g>
+                    <g>
+                      <path
+                        fill="#FFFFFF"
+                        d="M229.4,736.9H174c-1.1,0-2-0.9-2-2v-28.1c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v28.1
+                     C231.4,736,230.5,736.9,229.4,736.9z"
+                      />
+                      <path
+                        fill="#FFFFFF"
+                        d="M302.6,736.9h-55.3c-1.1,0-2-0.9-2-2v-28.1c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v28.1
+                     C304.6,736,303.7,736.9,302.6,736.9z"
+                      />
+                      <path
+                        fill="#FFFFFF"
+                        d="M375.8,736.9h-55.3c-1.1,0-2-0.9-2-2v-28.1c0-1.1,0.9-2,2-2h55.3c1.1,0,2,0.9,2,2v28.1
+                     C377.8,736,376.9,736.9,375.8,736.9z"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      </span>
       <div className={`${parseFloat(basketTotal) > parseFloat(paymentTaken) ? "" : "hidden"}`}>
         {parseFloat(basketTotal) > parseFloat(paymentTaken) && <AiOutlineLoading3Quarters className="animate-spin mx-auto text-5xl" />}
         {parseFloat(basketTotal) > parseFloat(paymentTaken) && <p className="text-center text-sm my-4">Awaiting Payment...</p>}
@@ -342,6 +597,7 @@ const Payment = ({ user, basketItems, setBasketItems, venueNtable, setVenueNtabl
           {stripeResponse.startsWith("Fail") ? "If you are a developer or a tester, you probably forgot to start the development server or the correct env." : ""}
         </div>
       )}
+    
     </div>
   );
 };
