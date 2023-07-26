41bf3b7de5e7fd2c3d30e2ada570e44fb29c230f
mihaicc30 <alemihai25@gmail.com>
2023-07-26 07:51:31 +0100

    added another possible development page

---
+++ src/comp/admin/AdminCourses.jsx
@@ -0,0 +1,18 @@
+
+
+const AdminCourses = () => {
+  return (
+    <div className="flex flex-col">
+    <p className="text-xl font-bold p-2 underline relative">
+      Courses <span className=" absolute text-xs font-light top-2">*in future development</span>
+    </p>
+       <div className="grow flex flex-wrap">
+
+       <div className="widget flex-1 p-2 m-1 shadow-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint atque, doloremque nulla quia officia consequatur expedita voluptates minus voluptatibus assumenda. Soluta, fugit suscipit ullam pariatur neque id est mollitia maiores!</div>
+
+       </div>
+    </div>
+  )
+}
+
+export default AdminCourses
