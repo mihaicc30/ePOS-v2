3c13f3a6ab62e7d2175305d7ffa210f257a561c2
mihaicc30 <alemihai25@gmail.com>
2023-08-15 10:09:23 +0100

    cleaned up code

--- routes/rota.js
+++ routes/rota.js
@@ -6,48 +6,37 @@ const Posusers = require("../models/posusers");
 router.post("/handleClocked", async (req, res) => {
   console.log(req.body);
   try {
-    // check if user, check if user is alkready roted, create new entry, update clocked out entry
     let query = await ROTA.findOne({ week: req.body.week, venueID: req.body.venueID });
     console.log(`Looking for ${req.body.week} ${req.body.email}`);
-    //if rota from this venue exists
     if (query) {
       let rotedData = query.roted;
       let tempData = query.roted[`${req.body.email}`][`${req.body.typeOfDay}`][`clocked`];
-      console.log("🚀", tempData, tempData.length);
-      console.log("🚀 ~ file: rota.js:18 ~ router.post ~ tempData:", tempData, tempData.length < 1, tempData >= 1);
       if (tempData.length < 1) {
-        console.log("usr hasnt clocked in at all for the day");
-        console.log(new Date().toLocaleTimeString().substring(0, 5));
-        rotedData[`${req.body.email}`][`${req.body.typeOfDay}`][`clocked`].push([`${new Date().toLocaleTimeString().substring(0, 5)} - ?`]);
+        rotedData[`${req.body.email}`][`${req.body.typeOfDay}`][`clocked`].push(`${new Date().toLocaleTimeString().substring(0, 5)} - ?`);
         let query2 = await ROTA.updateOne({ week: req.body.week, venueID: req.body.venueID }, { $set: { roted: rotedData } });
+        return res.status(200).json({ message: `${rotedData[`${req.body.email}`].displayName} has been clocked in.` });
       } else if (tempData.length >= 1) {
-        console.log("user has SOME clocke in data");
         let lastEntry = tempData[tempData.length - 1];
-        console.log("🚀 ~ file: rota.js:28 ~ router.post ~ lastEntry:", lastEntry);
         let startTime = lastEntry.split(" - ")[0];
         let endTime = lastEntry.split(" - ")[1];
         if (endTime === "?") {
-          console.log("Usr is partly clocked that means he wants to CLOCK OUT");
-          // update his entry to clock out
           endTime = new Date().toLocaleTimeString().substring(0, 5);
-          console.log("LE", tempData[tempData.length - 1]);
           tempData[tempData.length - 1] = `${startTime} - ${endTime}`;
-          console.log("LE", tempData[tempData.length - 1]);
-          console.log("EL", tempData);
           rotedData[`${req.body.email}`][`${req.body.typeOfDay}`][`clocked`] = tempData;
           let query3 = await ROTA.updateOne({ week: req.body.week, venueID: req.body.venueID }, { $set: { roted: rotedData } });
+          return res.status(200).json({ message: `${rotedData[`${req.body.email}`].displayName} has been clocked out.` });
         } else if (/^([01]\d|2[0-3]):([0-5]\d)$/.test(endTime)) {
-          console.log("Usr has been clocked in and out. Push new entry. Means he came back from this break.");
           rotedData[`${req.body.email}`][`${req.body.typeOfDay}`][`clocked`].push(`${new Date().toLocaleTimeString().substring(0, 5)} - ?`);
           let query4 = await ROTA.updateOne({ week: req.body.week, venueID: req.body.venueID }, { $set: { roted: rotedData } });
+          return res.status(200).json({ message: `${rotedData[`${req.body.email}`].displayName} has been clocked in.` });
         } else {
           console.log(tempData);
           console.log("Usr has some wierd data?! Do investigate logs.");
+          return res.status(400).json({ message: `Error. Do investigate logs.` });
         }
       }
     }
     console.log("User ROTA data has been updated.", new Date().toLocaleString());
-    res.status(200).json({ message: "ok" });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ message: error.message });
